6b07f4cfb6f5d0fa30fc3e39ee10adb0f11bcd2a
Fahim <chowdhuryfahimali@gmail.com>
2026-05-02 16:43:00 +0530

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    sitemap({
+      hostname: "https://cbsaon2026.com",
+    }),
+  ],
 });
